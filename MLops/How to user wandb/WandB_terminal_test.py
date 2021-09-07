19ba1e68830f6f21054caf868f667b809adf83d6
deokgu <onefence1994@gmail.com>
2021-09-07 15:27:12 +0900

    wandb test

--- MLops/How to user wandb/WandB_terminal_test.py
+++ MLops/How to user wandb/WandB_terminal_test.py
@@ -1,5 +1,10 @@
 import wandb
+import os 
 
 if __name__ == "__main__":
     wandb.login()
-    
+    # wandb.init() # 한번 저장하면 계속 된다.. 
+    os.system("2")
+
+
+    # key = "99410d7ae26f2b7a8d941ec09c9608207dcf987e"
